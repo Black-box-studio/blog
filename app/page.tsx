afd4e926389de6188abb2e5cf598bdd68ecbca56
Black-box-studio <vinothvin8012@gmail.com>
2024-03-22 01:15:51 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <section className="flex-start flex-col paddings mb-16">
+      <h1>Videos 1</h1>
+      <h1>Videos 2</h1>
+      <h1>Videos 3</h1>
+    </section>
+  );
+
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { NavLinks } from "@/constant";
+import { headers } from "next/headers";
+
+const Navbar = () => {
+    return (
+        <nav className="flex-between navbar">
+            <div className="flex-1 flex-start gap-10 items-center">
+                {/* Logo */}
+                <Link href="/">
+                    <Image
+                        src="./blackboys.svg"
+                        alt="BlackBoys Logo"
+                        width={116}
+                        height={43}
+                        />
+                </Link>
+                {/* Nav Links */}
+                <ul className=" justify-end flex-end gap-7 xl:flex items-center text-small">
+                    {NavLinks.map((link) => (
+                        <Link
+                            key={link.key}
+                            href={link.href}
+                            className="nav-link"
+                            >
+                            {link.text}
+                        </Link>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar
